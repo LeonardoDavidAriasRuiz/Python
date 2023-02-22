291dc221c56bfbc7e69b7d4b5e39501b512aa95b
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-22 17:31:40 -0600

    Functions of a string added.

--- Strings.py
+++ Strings.py
@@ -20,3 +20,37 @@ print(quote)
 
 quote = 'She said "Hello"'
 print(quote)
+
+#know if a string is inside of another string
+print("tuesday" in "monday, tuesday, wensday")
+
+#Size of a string
+print(len("Hello"))
+
+#Set the word in lower/uppercase
+print("Hello".upper())
+print("HEllo".lower())
+
+#Swap cases
+print("aaBBccDD".swapcase())
+
+#Set first letter of a string in uppercase
+print("leonardo david arias ruiz".capitalize())
+
+#Set first letter of each word of a string in uppercase
+print("leonardo david arias ruiz".title())
+
+#Count how many times is a string inside of another string
+print("Saturday".count("a"))
+
+#If a string starts / ends in a string
+print("Saturday".startswith("Sa"))
+print("Saturday".endswith("ay"))
+
+#Replace a part of the string into another
+print("Saturday".replace("a", "e"))
+
+#If a string can be an int
+print("a".isdigit())
+print("2".isdigit())
+print("2.2".isdigit())
