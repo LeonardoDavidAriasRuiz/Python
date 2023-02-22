fa89734240886ee649f5b736b2d35b13451a202e
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-22 15:57:00 -0600

    Transformation file added.

---
+++ Transformation.py
@@ -0,0 +1,29 @@
+
+month = "July"
+print(type(month))
+
+month = 7
+print(type(month))
+
+month = 1.1
+print(type(month))
+
+month = True
+print(type(month))
+
+number = 12
+print(type(number))
+print(number)
+
+number = str(12)
+print(type(number))
+print(number)
+
+number = int("12")
+print(type(number))
+print(number)
+
+#An error will happen
+#number = int("12sdf")
+#print(type(number))
+#print(number)
