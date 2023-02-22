34db08dbeffaeff98cdaa90e6d00b829e8a617fa
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-22 15:29:12 -0600

    Strings class added.

---
+++ Strings.py
@@ -0,0 +1,22 @@
+#Declaration
+firstName = "Leonardo"
+lastName = 'Arias'
+
+#link 2...n strings
+fullName = firstName + " " + lastName
+print(fullName)
+
+mon = "monday"
+tue = "tuesday"
+week = "Week: {}, {}.".format(mon,tue)
+print(week)
+
+week = f"Week days: {mon} and {tue}."
+print(week)
+
+#How to use "" or '' in a string
+quote = "I'm dancing"
+print(quote)
+
+quote = 'She said "Hello"'
+print(quote)
