a9febb2907ba52f33601d6607a69ba9a0a462799
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-22 17:48:31 -0600

    Indexing and slicing addet to Strings file.

--- Strings.py
+++ Strings.py
@@ -54,3 +54,31 @@ print("Saturday".replace("a", "e"))
 print("a".isdigit())
 print("2".isdigit())
 print("2.2".isdigit())
+
+#Get the character of a string
+print("abcdefghijk"[0])
+print("abcdefghijk"[1])
+print("abcdefghijk"[3])
+
+print("abcdefghijk"[-1])
+print("abcdefghijk"[-2])
+print("abcdefghijk"[-4])
+
+#Get the part of a string from a position to another         it doesn't get the las letter of the position
+print("0123456789"[0:5])
+print("0123456789"[:5])
+print("0123456789"[5:])
+print("0123456789"[5:-1])
+print("0123456789"[:])
+
+print("0123456789"[::1])
+print("0123456789"[::2])
+print("0123456789"[::3])
+
+print("0123456789"[5::1])
+print("0123456789"[5::2])
+print("0123456789"[5::3])
+
+print("0123456789"[:5:1])
+print("0123456789"[:5:2])
+print("0123456789"[:5:3])
