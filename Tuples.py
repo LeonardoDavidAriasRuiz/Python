e8319bb6000ffec8fde456426b0413f052647387
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-22 18:37:50 -0600

    Tuples file added.

---
+++ Tuples.py
@@ -0,0 +1,23 @@
+
+#######Just for read########       No updateable
+numbers = (1, 2, 3, 4, 5)
+strings = ("a", "b", "c")
+
+print(type(numbers))
+print(type(strings))
+
+print(numbers[0])
+print(strings[0])
+
+print(numbers[-1])
+print(strings[-1])
+
+##Pareable with a list
+
+list = list(numbers)
+print(list)
+
+list.append(6)
+
+tuple = tuple(list)
+print(tuple)
