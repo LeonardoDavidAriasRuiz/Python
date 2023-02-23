a1c41f6faa34cae63b31290f0d647474ed908335
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-22 18:13:36 -0600

    List file added.

---
+++ List.py
@@ -0,0 +1,21 @@
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
+print(numbers)
+print(type(numbers))
+
+print(numbers[6])
+print(numbers[0:3])
+print(numbers[:3])
+print(numbers[5:9])
+print(numbers[5:])
+
+print(numbers[::2])
+
+print(23 in numbers)
+
+differentsTypes = [0,"1", 2.3, True]
+print(differentsTypes)
+print(type(differentsTypes))
+
+print(True in differentsTypes)
+
