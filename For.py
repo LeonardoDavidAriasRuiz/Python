60e2d2fa23c929cb1dd26286059414c79ba35073
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-28 14:22:14 -0600

    For.py file edited.

--- For.py
+++ For.py
@@ -1,10 +1,13 @@
 
-for element in range(10): #Doesn't count the last number.
+#Doesn't count the last number.
+for element in range(10): 
     print(element)
 
-for element in range(1, 10): #Starts from 'a' until 'b', doesn't count the last number.
+#Starts from 'a' until 'b', doesn't count the last number.
+for element in range(1, 10): 
     print(element)
 
+#You can use Lists/ Arrays, Tuples, Dictionaries, etc.
 list = ["a", "b", "c", "d", "e", "f", "g"]
-for letter in list:
+for letter in list: 
     print(letter)
