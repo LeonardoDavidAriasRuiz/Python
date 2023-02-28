5d6bd4aa34281e255131073886c62b62cc1c7420
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-28 14:10:33 -0600

    For file midified.

---
+++ For.py
@@ -0,0 +1,10 @@
+
+for element in range(10): #Doesn't count the last number.
+    print(element)
+
+for element in range(1, 10): #Starts from 'a' until 'b', doesn't count the last number.
+    print(element)
+
+list = ["a", "b", "c", "d", "e", "f", "g"]
+for letter in list:
+    print(letter)
--- While.py
+++ While.py
@@ -1,15 +1,15 @@
 counter = 0
 
-# while counter <= 10:
-#     print(counter)
-#     counter += 1
+while counter <= 10:
+    print(counter)
+    counter += 1
 
-# counter = 0
-# while counter <= 10:
-#     print(counter)
-#     counter += 1
-#     if counter == 8:
-#         break
+counter = 0
+while counter <= 10:
+    print(counter)
+    counter += 1
+    if counter == 8:
+        break
  
 counter = 0
 while counter <= 10:
