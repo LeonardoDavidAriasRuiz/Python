51406c32a04424b8c5b906721d307c6b617aa565
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-28 17:50:03 -0600

    Sets.py file added.

---
+++ Sets.py
@@ -0,0 +1,15 @@
+
+# Can't have the same value more than one time
+# Random placed
+countries = {"Mexico", "Canada", "USA", "Mexico"}
+print(countries)
+
+
+string = set("Hola como estas")
+print(string)
+
+tuple = set(("abc", "def", "ghi", "abc"))
+print(tuple)
+
+list = set([1, 2, 3, 4, 5, 1, 2])
+print(list)
