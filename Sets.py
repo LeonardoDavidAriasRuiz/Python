94d1d896a1aaf3048e211e38740a9d2a79161bbd
Leonardo David Arias Ruiz <leonard_ar@icloud.com>
2023-02-28 18:00:09 -0600

    CRUD added to Sets.py.

--- Sets.py
+++ Sets.py
@@ -4,7 +4,7 @@
 countries = {"Mexico", "Canada", "USA", "Mexico"}
 print(countries)
 
-
+# Convert from one to sets
 string = set("Hola como estas")
 print(string)
 
@@ -13,3 +13,34 @@ print(tuple)
 
 list = set([1, 2, 3, 4, 5, 1, 2])
 print(list)
+
+# =====================
+#       C R U D
+# =====================
+
+
+# Read
+
+print(len(countries))
+print("USA" in countries)
+
+
+# Update
+
+countries.add("China")
+countries.add("China")
+print(countries)
+
+countries.update({"Spain", "Italy"})
+print(countries)
+
+# Delete
+
+countries.remove("China")
+print(countries)
+
+countries.discard("Japan")
+print(countries)
+
+countries.clear()
+print(countries)
